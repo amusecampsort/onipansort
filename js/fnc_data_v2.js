0949b4f94af757c5cf5e4616596634a4a95624a4
amusecampsort <108349245+amusecampsort@users.noreply.github.com>
2022-07-10 19:31:10 +0200

    a
    a

---
+++ js/fnc_data_v2.js
@@ -0,0 +1,43 @@
+﻿// 2008/7/3 Scripted by K-Factory@migiwa
+// 2009/1/27 Modified by K-Factory@migiwa
+
+// *****************************************************************************
+str_CenterT = 'Tie!';
+str_CenterB = 'Undo last choice';
+
+str_ImgPath = 'img/';
+// 0:順番に　1:昔の
+var bln_ResultMode = 1;
+// 0:テキスト　1:イラスト　2:テキスト＋イラスト
+var int_ResultImg = 2;
+// イラスト表示時、何位までをイラスト表示にするか。
+var int_ResultRank = 3;
+
+// ソート用のテーブルを
+// 0:残す　1:消す
+var bln_ResultStyle = 0;
+
+// ソート進捗バーの表示
+// 0:表示　1:消す
+var bln_ProgessBar = 1;
+
+// Maximum number of result rows before being broken off into another table.
+var maxRows = 3;
+
+// * タイトル情報（編集可能。最後の行に”,”を付けないようにしてください）
+var int_Colspan = 1;
+var ary_TitleData = [
+  "Current Members",
+];
+
+// * キャラクター情報（編集可能。最後の行に”,”を付けないようにしてください）
+// * 使用フラグ（0にするとソートに入りません）, 
+//   "タイトルID"（先頭から0, 1, 2...）, 
+//   {タイトル別参加フラグ}（1を入れると対象タイトルに入ります）,
+//   "キャラクター名", "画像（空白の場合、キャラクター名が使用されます）"
+//                                      [1,2,3,4,5,6,7,8,9,
+var ary_CharacterData = [
+  [1, "Nozaki Yume",        [1,0,0,0,0,0], "current/members_idx_nozaki.jpg"],   
+  [1, "Nonaka Kokona",      [1,0,0,0,0,0], "current/members_idx_nonaka.jpg"],   
+  [1, "Negishi Mika",       [1,0,0,0,0,0], "current/members_idx_todaka.jpg"],
+];
